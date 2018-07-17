c38a5b6e1763d7aa015c224107f8d5355d0d083d
Cristina Fernández <novedades@cristinafernandez.es>
2018-07-17 16:17:19 +0200

    Add files via upload

---
+++ JS/practice 10 JS/setinterval.js
@@ -0,0 +1,9 @@
+function sing () {
+console.log ("Love of my life, you've hurt me");
+console.log ("You've broken my heart and now you leave me.");
+console.log ("Love of my life can't you see.");
+console.log ("Bring it back, bring it back,");
+console.log ("Don't take it away from me because you don't know what it means to me.");
+}
+
+setInterval(sing,1000);
